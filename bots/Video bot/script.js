0c8d9b42aa477f20cf66987489b3f9b2ba06c1f5
Stailz-exe <ecsadee@gmail.com>
2026-03-01 16:44:50 +0500

    Исправлен backend и MiniApp для Render

--- bots/Video bot/script.js
+++ bots/Video bot/script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "https://YOUR_RENDER_URL"; // замените на URL Render
+const BACKEND_URL = "https://wytrex.onrender.com"; // замените на URL Render
 
 const checkBtn = document.getElementById("checkBtn");
 const downloadBtn = document.getElementById("downloadBtn");
